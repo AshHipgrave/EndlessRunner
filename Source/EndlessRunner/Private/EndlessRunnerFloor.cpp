75d91154b95410198a577ee3b2bba98f3bd1bcef
Ash Hipgrave <AshHipgrave@users.noreply.github.com>
2019-04-08 21:20:54 +0100

    Began adding support for the floor tiles which will form the course

---
+++ Source/EndlessRunner/Private/EndlessRunnerFloor.cpp
@@ -0,0 +1,29 @@
+// Copyright 2019 Ashley Hipgrave. All rights reserved.
+
+#include "EndlessRunnerFloor.h"
+#include "Components/SceneComponent.h"
+#include "Components/ArrowComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+AEndlessRunnerFloor::AEndlessRunnerFloor()
+{
+	SceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComponent"));
+	RootComponent = SceneComponent;
+
+	FloorMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FloorMeshComponent"));
+	FloorMesh->SetupAttachment(SceneComponent);
+
+	FloorMesh->SetWorldScale3D(FVector(1.f, 3.f, 1.f));
+	FloorMesh->SetRelativeScale3D(FVector(1.f, 3.f, 1.f));
+
+	AttachComponent = CreateDefaultSubobject<UArrowComponent>(TEXT("AttachComponent"));
+	AttachComponent->SetupAttachment(SceneComponent);
+}
+
+// Called when the game starts or when spawned
+void AEndlessRunnerFloor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
---
+++ Source/EndlessRunner/Public/EndlessRunnerFloor.h
@@ -0,0 +1,35 @@
+// Copyright 2019 Ashley Hipgrave. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EndlessRunnerFloor.generated.h"
+
+class UArrowComponent;
+class USceneComponent;
+class UStaticMeshComponent;
+
+UCLASS()
+class ENDLESSRUNNER_API AEndlessRunnerFloor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AEndlessRunnerFloor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USceneComponent* SceneComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* FloorMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UArrowComponent* AttachComponent;
+};
